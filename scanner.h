374dd183b77bef433d4c23da9778a572d643c248
isabelladominguez <105949248+isabelladominguez@users.noreply.github.com>
2022-08-15 20:29:35 -0400

    Create scanner.h

---
+++ scanner.h
@@ -0,0 +1,29 @@
+#include "token.h"
+#include <fstream>
+
+class Scanner 
+{
+    private: 
+        string input;//Armazena o texto de entrada
+        int pos;//Posição atual
+        int line;
+        
+        Token** tabKeywords;
+        void fillTable();
+        Token* search(string);
+    
+    public:
+    //Construtor
+        Scanner(string);
+
+        int getLine();
+    
+        //Método que retorna o próximo token da entrada
+        Token* nextToken();        
+    
+        //Método para manipular erros
+        void lexicalError(string);
+
+        //Destrutor
+        ~Scanner();
+};
